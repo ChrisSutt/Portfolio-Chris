4fc02fd6485f12aa0c6327c1623e195d28e2a9a1
Chris.Sutton <sutt.chris@outlook.com>
2024-11-28 18:34:25 -0400

    updated about.jsx with tailwind css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,27 @@
 import { motion } from 'framer-motion';
 
 const Hero = () => (
-    <section className ="h-screen flex flex-col justif-center items-center bg-gradient-to-b from-blue-500 to-indigo-600 text-white">
+    <section id="hero" className ="bg-gradient-to-r from-purple-500 to-blue-500 text-white text-center py-32 flex flex-col items-center">
         <motion.h1
-            initial = {{ opacity: 0, y: -50 }}
+            initial = {{ opacity: 0, y: -20 }}
             animate = {{ opacity: 1, y: 0 }}
-            transition ={{ duration: 0.5 }}
-            className="text 5xl md:text-7xl font-bold"
+            transition ={{ duration: 0.8 }}
+            className="text-4xl font-bold"
             >
                 Hi, I'm <span className="text-yellow-300">Chris</span>
             </motion.h1>
-            <p className="mt-4 text-lg md:text-2xl">A Web Developer & Software Engineer</p>
-            <button onClick={() => window.scrollTo(0, document.body.scrollHeight)}>
-                Contact Me
-            </button>
+            <p className="mt-4 text-lg">A Web Developer | Software Engineer</p>
+        <motion.botton 
+            initial={{ scale: 0.9 }}
+            animate={{ scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            className="mt-10 px-6 py-3 bg-yellow-400 text-black font-semibold rounded shadow hover:bg-yellow-500 border"
+            onClick-={() => 
+                document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })
+            }
+        >
+            Contact Me 
+        </motion.botton>
     </section>
 );
 
