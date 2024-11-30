65023a87aa0ad7e81d545c700a6ab919c6017a25
Chris.Sutton <sutt.chris@outlook.com>
2024-11-30 00:30:03 -0400

    updated About me section more. Needs more styling / seperation between text / skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { motion, useInView } from 'framer-motion'; 
-import { FaReact, FaNodeJs, FaJsSquare, FaHtml5, FaCss3Alt, FaGitAlt, FaDatabase, FaFigma, } from 'react-icons/fa';
+import { FaReact, FaNodeJs, FaJsSquare, FaHtml5, FaCss3Alt, FaGitAlt, FaDatabase, FaFigma, FaJs, FaBug, FaExchangeAlt, FaTasks, FaDesktop } from 'react-icons/fa';
 import { useRef } from 'react';
 
 const About = () => {
@@ -26,12 +26,14 @@ const About = () => {
         ></motion.div>
             <h2 className="text-4xl font-bold text-blue-600 mb-4">Who I Am</h2>
 
+            
+
                 {/*Left Section */}
                 <motion.div
                     variants={leftVariants}
                     initial="hidden"
                     animate={inView ? 'visible' : 'hidden' }
-                    className="text-center max-w-2xl space-y-4 px-4"
+                    className="text-left items-center max-w-2xl space-y-4 px-4"
                     >
                         <h3 className="text-2xl font-semibold"></h3>
                         <p>
@@ -51,47 +53,125 @@ const About = () => {
                         </p>
         </motion.div>
 
-    
-                <motion.div
-                    variants={rightVariants}
-                    initial="hidden"
-                    animate={inView ? 'visible' : 'hidden'}
-                    className="grid grid-cols-2 md:grid-cols-6 gap6 mt-12"
-                >
-                    {/* Skill Icons */}
-                    <div className="flex flex-col items-center">
-                        <FaReact className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">React</span>
+    <div className="relative w-full py-16 bg-purple600 rounded-t-xl">
+        <motion.div
+        
+            variants={rightVariants}
+            initial="hidden"
+            animate={inView ? 'visible' : 'hidden'}
+            className="grid grid-cols-1 md:grid-cols-3 gap-1 mt-12 w-3/4 mx-auto">
+                {/*front end */}
+                <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
+                    <img
+                        src="https://fakeimg.pl/20x20"
+                        alt="Frontend Icon"
+                        className="w-20 h-20 mb-4"
+                    />
+                    <h3 className="text-2xl font-bold text-blue-600 mb-2">Frontend</h3>
+                    <p className="text-gray-600 mb-4 text-center">
+                        Specializing in responsive design, React Development, and crafting engaging user interfaces.
+                    </p>
+                    <div className="flex flex-col space-y-2">
+                    <h3 className="text-1xl font-bold text-blue-600 mb-2 text-center">Tools</h3>
+                        <div className="flex items-center">
+                            <FaReact className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">React</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FaJsSquare className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">JavaScript</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FaHtml5 className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">Html5</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FaCss3Alt className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">CSS</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FaGitAlt className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">Github</span>
+                        </div>
+                                                <div className="flex items-center">
+                            <FaDatabase className="text-blue-500 text-5xl mr-2" />
+                            <span className="text-xl font-semibold text-gray-700">jQuery</span>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaNodeJs className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">NodeJs</span>
+                </div>
+
+                {/* backend */}
+                <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
+                <img
+                        src="https://fakeimg.pl/20x20"
+                        alt="Frontend Icon"
+                        className="w-20 h-20 mb-4"
+                    />
+                    <h3 className="text-2xl font-bold text-green-600 mb-2">Backend</h3>
+                    <p className="text-gray-600 mb-4 text-center">
+                    Focused on building scalable server-side applications and managing databases efficiently.
+                    </p>
+                    <h3 className="text-1xl font-bold text-green-600 mb-2 text-center">Tools</h3>
+                    <div className="flex flex-col space-y-2">
+                    <div className="flex items-center">
+                        <FaNodeJs className="text-green-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Node</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaJsSquare className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">JsSquare</span>
+                    <div className="flex items-center">
+                        <FaDatabase className="text-green-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">MySql</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaHtml5 className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">Html5</span>
+                    <div className="flex items-center">
+                        <FaDatabase className="text-green-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">NoSql</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaCss3Alt className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">CSS3</span>
+                    <div className="flex items-center">
+                        <FaDatabase className="text-green-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">MongoDB</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaGitAlt className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">Github</span>
+                    <div className="flex items-center">
+                        <FaDatabase className="text-green-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Express.js</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaDatabase className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">Databases</span>
+                </div>
+            </div>
+
+                <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
+                <img
+                        src="https://fakeimg.pl/20x20"
+                        alt="Frontend Icon"
+                        className="w-20 h-20 mb-4"
+                    />
+                    <h3 className="text-2xl font-bold text-purple-600 mb-2">UI/UX</h3>
+                    <p className="text-gray-600 mb-4 text-center">
+                    Dedicated to designing intuitive and engaging user experiences and prototypes.
+                    </p>
+                    <div className="flex flex-col space-y-2">
+                    <h3 className="text-1xl font-bold text-purple-600 mb-2 text-center">Tools</h3>
+                    <div className="flex items-center">
+                        <FaFigma className="text-purple-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Figma</span>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <FaFigma className="text-blue-500 text-5xl" />
-                        <span className="mt-2 text-sm font-semibold">Figma</span>
+                    <div className="flex items-center">
+                        <FaDesktop className="text-purple-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Interfaces</span>
                     </div>
-                </motion.div>
+                    <div className="flex items-center">
+                        <FaBug className="text-purple-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Debugging</span>
+                    </div>
+                    <div className="flex items-center">
+                        <FaTasks className="text-purple-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Project Dev</span>
+                    </div>
+                    <div className="flex items-center">
+                        <FaExchangeAlt className="text-purple-500 text-5xl mr-2" />
+                        <span className="text-xl font-semibold text-gray-700">Rest APIs</span>
+                    </div>
+                </div>
+            </div>
+            </motion.div>
+        </div>
         </section>
     );
 };
